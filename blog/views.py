1efc4de96bc6caee47e08706449e1ba13d7d6546
Krijan Niroula <krijan.segonatech@gmail.com>
2019-09-26 09:55:03 +0545

    Complete

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from blog.models import Post, Comment
 from blog.forms import CommentForm
+
 def blog_index(request):
     posts = Post.objects.all().order_by('-created_on')
     context={
@@ -38,6 +39,7 @@ def blog_detail(request,pk):
     context = {
         "post":post,
         "comments":comments,
+        "form":form,
     }
     return render(request,"blog_detail.html",context)
 
